73f52b9faeab3bbf288f5f5d363cbf2c6597ede1
Jesse Brown <rudy@Craigs-MacBook-Pro.local>
2024-08-11 16:46:28 -0400

    test1

--- python-fundamentals/imports.py
+++ python-fundamentals/imports.py
@@ -1,4 +1,6 @@
 from functions import example       # Simple import - Class is function and the method is example
+import sys     #  the path that python goes into to look for modules
 
+#example()
 
-example()
+print(f"\n PythonThe System Path is: {sys.path} \n")
